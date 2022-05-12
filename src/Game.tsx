3b4d17442e8e9b6d7cbd8dba774678e53b9f2b19
grantbw4 <grantbw4@gmail.com>
2022-05-12 17:00:51 -0400

    blurtle.one

--- src/Game.tsx
+++ src/Game.tsx
@@ -150,7 +150,7 @@ function Game(props: GameProps) {
   // Function to share URL of game
 
   async function share(copiedHint: string, text?: string) {
-    const url = "grantbw4.github.io/blurtle/"
+    const url = "blurlte.one"
     const body = url + (text ? "\n\n" + text : "");
     if (
       /android|iphone|ipad|ipod|webos/i.test(navigator.userAgent) &&
